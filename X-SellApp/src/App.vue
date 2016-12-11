7ffab6bf23c3d706e121cf8f6a185c3c1f597ca6
littlewin <littlewin.wang@gmail.com>
2016-12-11 17:07:43 +0800

    Add demo of sellapp

---
+++ X-SellApp/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <v-header></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <a v-link="{path: '/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/seller'}">商家</a>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+
+  export default {
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel=stylesheet/stylus>
+  @import "./common/stylus/mixin.styl"
+
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 100
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
+</style>

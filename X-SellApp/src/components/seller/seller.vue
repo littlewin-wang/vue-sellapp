9fc3f45b417e4911e6623677ff57b324f9d63325
littlewin <littlewin.wang@gmail.com>
2016-12-18 08:15:47 +0800

    update seller pics

--- X-SellApp/src/components/seller/seller.vue
+++ X-SellApp/src/components/seller/seller.vue
@@ -42,6 +42,17 @@
           </li>
         </ul>
       </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" v-el:picwrapper>
+          <ul class="pic-list" v-el:piclist>
+            <li class="pic-item" v-for="pic in seller.pics">
+              <img :src="pic" width="120" height="90">
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -64,10 +75,12 @@
     watch: {
       'seller' () {
         this._initScroll()
+        this._initPics()
       }
     },
     ready () {
       this._initScroll()
+      this._initPics()
     },
     methods: {
       _initScroll () {
@@ -77,6 +90,24 @@
           })
         } else {
           this.scroll.refresh()
+        }
+      },
+      _initPics () {
+        if (this.seller.pics) {
+          let picwidth = 120
+          let margin = 6
+          let width = (picwidth + margin) * this.seller.pics.length - margin
+          this.$els.piclist.style.width = width + 'px'
+          this.$nextTick(() => {
+            if (!this.picScroll) {
+              this.picScroll = new BScroll(this.$els.picwrapper, {
+                scrollX: true,
+                eventPassthrough: 'vertical'
+              })
+            } else {
+              this.picScroll.refresh()
+            }
+          })
         }
       }
     },
@@ -184,4 +215,24 @@
             line-height: 16px
             font-size: 12px
             color: rgb(7, 17, 27)
+    .pics
+      padding: 18px
+      .title
+        margin-bottom: 12px
+        line-height: 14px
+        color: rgb(7, 17, 27)
+        font-size: 14px
+      .pic-wrapper
+        width: 100%
+        overflow: hidden
+        white-space: nowrap
+        .pic-list
+          font-size: 0
+          .pic-item
+            display: inline-block
+            margin-right: 6px
+            width: 120px
+            height: 90px
+            &:last-child
+              margin: 0
 </style>

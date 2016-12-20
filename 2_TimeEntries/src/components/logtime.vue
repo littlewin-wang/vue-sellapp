7882f84489746bb4e1b748b6518ac2579bf3b466
littlewin <littlewin.wang@gmail.com>
2016-12-20 18:28:18 +0800

    update

---
+++ 2_TimeEntries/src/components/logtime.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="form-horizontal">
+    <div class="form-group">
+      <div class="col-sm-6">
+        <label>日期</label>
+        <input
+         type="date"
+         class="form-control"
+         v-model="date"
+         placeholder="Date"
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="css" rel="stylesheet">
+
+</style>

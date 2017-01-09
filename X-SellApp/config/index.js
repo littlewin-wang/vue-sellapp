f0a01f8b5c6167e147ccfec9387c8fbc3ffbbfd2
littlewin <littlewin.wang@gmail.com>
2017-01-09 23:20:28 +0800

    update

--- X-SellApp/config/index.js
+++ X-SellApp/config/index.js
@@ -15,7 +15,7 @@ module.exports = {
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
-    port: 9000
+    port: 80
   },
   dev: {
     env: require('./dev.env'),
